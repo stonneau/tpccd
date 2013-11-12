8769428ad231aa56f78518aaf7405f28cf729b47
steve tonneau <stevetonneau@hotmail.fr>
2013-11-12 14:56:26 +0100

    cleaning

--- tp/student/Assets/TargetController.cs
+++ tp/student/Assets/TargetController.cs
@@ -6,15 +6,16 @@ public class TargetController : MonoBehaviour {
 	public Transform target;
 	/*Control target with directional keys + V and B*/
 	void Update () {
-		gameObject.transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10, 0, 0);
-		gameObject.transform.Translate(0, Input.GetAxis("Vertical") * Time.deltaTime * 10, 0);
+		float delta = Time.deltaTime * 10;
+		gameObject.transform.Translate(Input.GetAxis("Horizontal") * delta, 0, 0);
+		gameObject.transform.Translate(0, Input.GetAxis("Vertical") * delta, 0);
 		if(Input.GetKey(KeyCode.V))
 		{
-			gameObject.transform.Translate(0, 0, Time.deltaTime * 10);
+			gameObject.transform.Translate(0, 0, delta);
 		}
 		if(Input.GetKey(KeyCode.B))
 		{
-			gameObject.transform.Translate(0, 0, -Time.deltaTime * 10);
+			gameObject.transform.Translate(0, 0, -delta);
 		}
 	}
 }
